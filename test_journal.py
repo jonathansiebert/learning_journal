ff9502541b179e7a1705d54252b9726f5c82832e
Jonathan Siebert <jon.siebert@gmail.com>
2014-06-04 12:53:12 -0700

    New test file

---
+++ test_journal.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from contextlib import closing
+import pytest
+import os
+
+from journal import app
+from journal import connect_db
+from journal import get_database_connection
+from journal import init_db
+
+
+TEST_DSN = 'dbname=test_learning_journal user=' + os.environ.get('USER')
+
+
+def clear_db():
+    with closing(connect_db()) as db:
+        db.cursor().execute("DROP TABLE entries")
+        db.commit()
+
+pytest.fixture(scope='session')
+def test_app():
+    """configure our app for use in testing"""
+    app.config['DATABASE'] = TEST_DSN
+    app.config['TESTING'] = True
+
+@pytest.fixture(scope='session')
+def db(test_app, request):
+    """initialize the entries table and drop it when finished"""
+    init_db()
+
+    def cleanup():
+        clear_db()
+
+    request.addfinalizer(cleanup)
